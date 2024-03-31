9aa7f421a22f7d0785b369c50be15f4edb5acf07
Viktor Ferenczi <viktor@ferenczi.eu>
2024-03-31 16:47:27 +0200

    Added the source of ModDebugBuild plugin (avaness) and URLs to an old mod debugger from 2015

---
+++ Shared/ModDebugBuild/Main.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using Sandbox.Game.World;
+using System.Reflection;
+using VRage.FileSystem;
+using VRage.Game.Models;
+using VRage.Plugins;
+using VRage.Utils;
+
+namespace avaness.ModDebugBuild
+{
+    public class Main : IPlugin
+    {
+        private static string modPath;
+
+        public void Dispose()
+        {
+            MySession.OnUnloaded -= MySession_OnUnloaded;
+        }
+
+        public void Init(object gameInstance)
+        {
+            modPath = MyFileSystem.ModsPath.Replace('/', '\\');
+
+            Harmony harmony = new Harmony("avaness.ModDebugBuild");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            MySession.OnUnloaded += MySession_OnUnloaded;
+        }
+
+        private void MySession_OnUnloaded()
+        {
+            MyLog.Default.WriteLine("Unloading modded models.");
+            MyModels.UnloadModdedModels();
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public static bool IsLocalMod(string filename)
+        {
+            return filename.Replace('/', '\\').StartsWith(modPath);
+        }
+    }
+}
